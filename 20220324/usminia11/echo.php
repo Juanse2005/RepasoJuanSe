e95094fc9d2395c52ccaf3710ec81b538fe31cde
Juanse2005 <101894241+Juanse2005@users.noreply.github.com>
2022-03-24 17:26:58 -0500

    Add files via upload
    Ejercicios en php

---
+++ 20220324/usminia11/echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+ <h1>Hola soy un titulo</h1>
+ <?php
+//INICIA CODIGO PHP
+//Esto es un comentario
+    /*Todo lo que esté aqui dentro
+    se conseidera comentario
+    */
+    //COMANDO ECHO DEVUELVE UN VALOR
+    echo "Hola soy un parrafo";
+    echo "<br>"; //salto de linea
+    echo 3+3; /*realiza operaciones en php
+                que no se puede en html.*/
+//TERMINA PHP
+?>
+</body>
+</html>
---
+++ 20220324/usminia11/variables.php
@@ -0,0 +1,11 @@
+<?php
+//Definimos una variable con un texto
+$miVariable = "Hola soy una variable";
+//Visualizamos la variable en el explorador
+echo $miVariable;
+//Otro ejemplo variable miVariable igual a 2
+echo "<br>";
+$miValor= 2;
+//visualizamos sin embargo realizamos una mutiplicacion *3
+echo 3*$miValor;
+?>
